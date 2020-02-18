133232aacd1885220bcc5683dc8b03746abf466a
i1i1 <vanyarybin1@live.ru>
2020-02-19 00:12:51 +0300

    Add scripts and dwm autostart

---
+++ .dwm/autostart.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+rerun() {
+    killall -9 "$1" 2>&1 >/dev/null
+    "$@" &
+}
+
+run_one() {
+	if ! pgrep "$1" 2>&1 >/dev/null; then
+		"$@" &
+	fi
+}
+
+
+for i in firefox telegram-desktop thunderbird "hydroxide imap" "hydroxide smtp"; do
+    run_one $i
+done
+
+for i in usbd pingd dunst sxhkd; do
+    rerun $i
+done
+
+xrdb -merge ~/.Xresources
+setxkbmap -layout us,ru -option grp:alt_shift_toggle
+xmodmap ~/.Xmodmap
+/etc/cron.hourly/unsplash
+
+
+while xsetroot -name "$(statusbar)"; do
+	sleep 1
+done
