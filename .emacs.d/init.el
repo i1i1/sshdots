bbc2980d46f880cc1e36ff3340d1a9fc3bdd7d6a
i1i1 <vanyarybin1@live.ru>
2021-03-31 14:00:18 +0300

    Add lsp for emacs
    Signed-off-by: i1i1 <vanyarybin1@live.ru>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,3 +21,6 @@
 
 (use-package ob-async)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(load-file (let ((coding-system-for-read 'utf-8))
+                (shell-command-to-string "agda-mode locate")))
